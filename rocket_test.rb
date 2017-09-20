450ec89c8af93d102de341a5cc66a30394624418
Marlon O'Neil <hello@marlononeil.com>
2017-09-20 18:57:56 -0400

    Successfully tested the 'name=()' method

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,13 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+
+  def test_name=()
+    rocket = Rocket.new
+    rocket.name = "Test Name"
+    assert_equal "Test Name", rocket.name
+
+  end
+
+
 end
